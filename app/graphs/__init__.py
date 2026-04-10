d4db62c8f2443bc01c3d3abd9a6198390310b033
isabellabath1-arch <isabellabath1@gmail.com>
2026-04-10 17:33:57 +0100

    wipe branch

--- app/graphs/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('graphs',__name__)
-
-from app.graphs import routes
--- passenger_wsgi.py
+++
@@ -1,7 +0,0 @@
-import sys, os
-
-sys.path.insert(0, os.path.dirname(__file__))
-
-from app import create_app  # adjust this import to match your app
-
-application = create_app()
